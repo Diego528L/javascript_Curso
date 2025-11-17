4a04d4769f1f327d834818c4f907940040139298
Diego Leandro Alves <1042210@senacminas.edu.br>
2025-11-17 17:02:35 -0300

    arquivo ex11

--- ex10.js
+++ ex10.js
@@ -1,1 +1,9 @@
 //Meu doc inicial
+const prompt = require('prompt-sync')({ sigint: true });
+let numeros = [], i
+for (i = 0; i <= 10; i++) {
+    let n = Number(prompt("Digite um numero", i))
+    numeros[i] = n
+
+}
+console.log(numeros)
---
+++ ex11.js
@@ -0,0 +1,4 @@
+const prompt= require('prompt-sync')({sigint:true});
+let i
+for(i=0;i<=10;i++)
+console.log(i)
