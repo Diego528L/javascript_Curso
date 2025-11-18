d0cfe062c6913862982d67046e67724577b428d4
Diego Leandro Alves <1042210@senacminas.edu.br>
2025-11-18 16:54:00 -0300

    exforEach.js

---
+++ exForeach.js
@@ -0,0 +1,14 @@
+//sintase basica
+Array.forEach((elemento,indice) =>{
+
+});
+//exemplos praticos
+let games = ["Minecrafit", "Fortnite", "Roblox"];
+//mostrar cada game com emoji
+games.forEach((game,index) =>{
+    console.log(`${index + 1}.${game}`);
+});
+// resultado:
+// 1. Minecraft
+// 2. Fortnite
+// 3.Roblox
---
+++ exPop.js
@@ -0,0 +1,5 @@
+let lanches = ["xBurguer","Pizza","Sushi","Açai"];
+//Removeo ultimo elemento
+let removido = lanches.pop();
+console.log(removido);//"Acai"
+console.log(lanches);//["X Burguer","Pizza","Sushi"]
