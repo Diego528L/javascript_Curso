11f87479c1421c5c1d8a67e4259170facfa82a3f
Diego Leandro Alves <1042210@senacminas.edu.br>
2025-11-18 15:30:00 -0300

    exPush.js

---
+++ exPush.js
@@ -0,0 +1,8 @@
+let jogos = ["Minecrafit","Fortnite"];
+//Adiciona uma elemento no Final
+jogos.push("Roblox");
+console.log(jogos);//["Minecrafit","Fortnite","Roblox"]
+
+//Adiciona varios de uma vez
+jogos.push("Among Us","FiFa");
+console.log(jogos);//["Minecrafit","Fortnite","Roblox","Among Us","FiFa"]
