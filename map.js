ee91150a9a527a89d5fd190752da13bf42adab3c
Diego Leandro Alves <1042210@senacminas.edu.br>
2025-11-18 17:09:00 -0300

    map.js

---
+++ map.js
@@ -0,0 +1,10 @@
+//exemplos praticos:
+let numeros = [1, 2, 3, 4, 5,];
+
+//dobrar numeros
+let dobrados = numeros.map((numero) => {
+    returnnumero * 2;
+
+});
+console.log(dobrados);//[2, 4, 6, 8, 10]
+console.log(numeros);//[1, 2, 3, 4, 5,]
