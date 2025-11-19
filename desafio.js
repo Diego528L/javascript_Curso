c829a42ba3b7fa2e4f53fa354182861893ebdccb
Diego Leandro Alves <1042210@senacminas.edu.br>
2025-11-19 14:16:51 -0300

    desafio.js

---
+++ desafio.js
@@ -0,0 +1,9 @@
+let playlist = ["Dua lipa - Dance The Nigth","The Weeknd - Blinding Lights"];
+playlist.push("Vai - Ana Carolina", "Tailandia - Luiza Martins")
+console.log(playlist);
+let removido =playlist.pop();
+console.log(remmovido);
+console.log(playlist);
+playlist.forEach((playlist,index) =>{
+    console.log(`${index + 1} `);
+});
